87371388f35b3386dc9a9cdc16de81352675efde
AlbertLin <chlin701005@gmail.com>
2022-05-16 14:16:41 +0800

    springboot database practice

---
+++ src/main/java/com/uuu/springboot/demo/demo3/MyRunner1.java
@@ -0,0 +1,26 @@
+package com.uuu.springboot.demo.demo3;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class MyRunner1 implements CommandLineRunner {
+    @Autowired
+    NamedParameterJdbcTemplate template;
+
+    private final String SQL1 = "SELECT 888";
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("jdbc template:{}", template.getJdbcTemplate().getDataSource());
+        SqlParameterSource source = new MapSqlParameterSource();
+        Integer result = template.queryForObject(SQL1, source, Integer.class);
+        log.info("query from {}, result is {}", SQL1, result);
+    }
+}
